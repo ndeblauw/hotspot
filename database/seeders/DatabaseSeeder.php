edb0fad9b0dd6597b1ff271e2fcd01c4d68928c4
Nico Deblauwe <nico@bluepundit.eu>
2026-09-03 14:34:59 +0200

    FEATURE :sparkles: Add basic Article model functionalities

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Article::factory(10)->create();
         // User::factory(10)->create();
 
         User::factory()->create([
