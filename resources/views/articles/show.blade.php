ec5aeab7fd1356d1eab68736eae931a62e872df2
Nico Deblauwe <nico@bluepundit.eu>
2026-09-03 15:34:53 +0200

    FEATURE :sparkles: Add public Articles show

---
+++ resources/views/articles/show.blade.php
@@ -0,0 +1,10 @@
+<div>
+    Header comes here
+</div>
+
+<h1>{{$article->title}}</h1>
+<p><i>Author: {{$article->author}}</i></p>
+<p>{{$article->content}}</p>
+<div>
+    Footer comes here
+</div>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [WelcomeController::class, 'index'])->name('home');
 
 Route::get('articles', [ArticleController::class, 'index']);
+Route::get('articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
 
 
 
