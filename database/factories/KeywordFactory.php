f63473fef4be5f0eddc335b9e97aa5ad7a4c103c
Nico Deblauwe <nico@bluepundit.eu>
2026-09-26 15:20:47 +0200

    FEATURE :sparkles: Add Keyword model

---
+++ database/factories/KeywordFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Keyword;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Keyword>
+ */
+class KeywordFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+        ];
+    }
+}
