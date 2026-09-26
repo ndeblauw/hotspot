fbb16180d146e7ac10e2a8c3653e30952f33736f
Nico Deblauwe <nico@bluepundit.eu>
2026-09-26 15:30:37 +0200

    FEATURE :sparkles: Add Article-Keyword relation

---
+++ database/migrations/2026_09_26_132207_create_article_keyword_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('article_keyword', function (Blueprint $table) {
+            $table->foreignId('article_id');
+            $table->foreignId('keyword_id');
+        });
+    }
+};
